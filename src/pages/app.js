231ba83dbffb966de5d2f1f399037c86b27d0968
unknown <amirwebsajjad@gmail.com>
2025-06-25 13:07:46 +0330

    is it ok?

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,15 +1,15 @@
 window.addEventListener("load", () => {
-  var btn = document.getElementById("btn");
-  var defferdprompt;
-  window.addEventListener("beforeinstallprompt", function (event) {
-    console.log("first");
-    event.preventDefault;
-    defferdprompt = event;
-    return false;
-  });
-  btn.addEventListener("click", () => {
-    defferdprompt.prompt();
-  });
+  // var btn = document.getElementById("btn");
+  // var defferdprompt;
+  // window.addEventListener("beforeinstallprompt", function (event) {
+  //   console.log("first");
+  //   event.preventDefault;
+  //   defferdprompt = event;
+  //   return false;
+  // });
+  // btn.addEventListener("click", () => {
+  //   defferdprompt.prompt();
+  // });
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("sw.js")
